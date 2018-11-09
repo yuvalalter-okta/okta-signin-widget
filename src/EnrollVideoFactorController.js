1cab2d61d90e020b0324d3adf6e5473a79c1db30
Ken Wang <ken.wang@okta.com>
2018-11-08 21:34:40 -0500

    face

--- src/EnrollVideoFactorController.js
+++ src/EnrollVideoFactorController.js
@@ -11,57 +11,32 @@ function (Okta, FormController, Footer) {
   return FormController.extend({
     className: 'enroll-video-factor',
     Model: {
+      props: {
+        phrase: 'string'
+      },
       local: {
-        provider: 'string',
-        factorType: 'string'
+        phrases: 'array'
       },
       save: function () {
-        return this.manageTransaction((transaction, setTransaction) => {
-          var factor = _.findWhere(transaction.factors, {
-            provider: this.get('provider'),
-            factorType: this.get('factorType')
-          });
-          return factor.enroll()
-          .then((trans) => {
-            setTransaction(trans);
-            var url = this.appState.get('enrollVideoFactorRedirectUrl');
-            if(url !== null) {
-              Util.redirect(url);
-            }
-          })
-          .fail(function (err) {
-            throw err;
-          });
+        return this.doTransaction(function(transaction) {
+          var factor;
+          if (!!transaction.factors) {
+            factor = transaction.factors.find(function(factor) { return factor.factorType === 'bio:face' });
+          } else {
+            factor = transaction.factor;
+          }
+          return factor.enroll({});
         });
       }
     },
 
-    Form: function() {
-      var factors = this.options.appState.get('factors');
-      var factor = factors.findWhere({
-        provider: this.options.provider,
-        factorType: this.options.factorType
-      });
-      var vendorName = factor.get('vendorName');
-      var subtitle = Okta.loc('enroll.customFactor.subtitle', 'login', [vendorName]);
-      var saveText = Okta.loc('enroll.customFactor.save', 'login');
-      return {
-        autoSave: true,
-        title: vendorName,
-        subtitle: subtitle,
-        save: saveText,
-      };
-    },
-
-    trapAuthResponse: function () {
-      if (this.options.appState.get('isMfaEnrollActivate')) {
-        return true;
+    Form: {
+      autoSave: true,
+      title: 'Face Biometrics',
+      save: 'Enroll',
+      inputs: function () {
+        return [];
       }
-    },
-
-    initialize: function () {
-      this.model.set('provider', this.options.provider);
-      this.model.set('factorType', this.options.factorType);
     },
 
     Footer: Footer
